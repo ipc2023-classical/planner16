a99290dbecbc68a29628a1d822a0644c50213753
Daniel Gnad <daniel.gnad@liu.se>
2023-03-24 15:38:14 +0100

    prepare skeleton for satisficing portfolio + increase memory limit to 8000M

--- driver/portfolios/seq_sat_saarlumniplan.py
+++ driver/portfolios/seq_sat_saarlumniplan.py
@@ -2,64 +2,45 @@ SAS_FILE = "output.sas"
 OPTIMAL = False
 
 CONFIGS_STRIPS =  [
-    # decstar:ds-sss-oss
-    (79, ['fast-downward-decoupled', '--decoupling', 'lp_general(factoring_time_limit=30, memory_limit=7000, add_cg_sccs=true, strategy=mm_approx, min_flexibility=0.8)', '--search', 'lazy_greedy(ff, bound=BOUND)']),
-    # scorpion:sys-scp-interesting_non_negative-60s-cartesian-single
-    (898, ['fast-downward-conjunctions',  '--heuristic', 'hlm_normalcost=lmcount(lm_rhw(reasonable_orders=true))',
-    '--heuristic', 'hff_normalcost=ff(cache_estimates=false, cost_type=PLUSONE)',
-    '--search', 'ipc18_iterated([{}])'.format(
-        'lazy_iterated_weights_c([hff_normalcost, hlm_normalcost], preferred=[hff_normalcost], cached_heuristic=hff_normalcost, bound=BOUND)'
-    )]),
+    # decoupled search: inverted-fork factorings
+    ((50, 50), ['fast-downward-decoupled', "--decoupling",
+           "ifork(search_type=sat, max_leaf_size=100000)",
+           "--heuristic",
+           "hff=ff(cost_type=one)",
+           "--search",
+           "lazy_greedy([hff], "
+           "      cost_type=one, bound=BOUND,"
+           "      preferred=[hff])"]),
+    # decoupled search: fork factorings
+    ((0, 50), ['fast-downward-decoupled', "--decoupling",
+           "fork(search_type=sat, pruning=cost_frontier(irrelevance=TRANSITIONS), max_leaf_size=100000)",
+           "--heuristic",
+           "hff=ff(cost_type=one)",
+           "--search",
+           "lazy_greedy([hff], "
+           "      cost_type=one, bound=BOUND,"
+           "      preferred=[hff])"]),
+    # TODO GBFS-SCL (hCFF)
+    ((180, 1000), ['fast-downward-conjunctions',  'TODO']),
+    # decoupled search: general factorings
+    ((0, 600), ['fast-downward-decoupled', "--decoupling", # => skipped if solution has been found before
+           "lp_general(search_type=sat, factoring_time_limit=30, memory_limit=7500, add_cg_sccs=true, strategy=mm_approx, min_flexibility=0.8)",
+           "--heuristic",
+           "hff=ff(cost_type=one)",
+           "--search",
+           "lazy_greedy([hff],"
+           "            preferred=[hff], bound=BOUND, cost_type=one)"]),
+    # TODO LAMA-hCFF
+    ((1570, 100), ['fast-downward-conjunctions',  'TODO']),
 ]
 
-CONFIGS_COND_EFFS = [
-    # symk:sym-bd
-    (1251, ['symk', '--search', 'sym-bd(silent=true)']),
-    # scorpion:astar-blind
-    (5, ['scorpion', '--search', 'astar(blind())']),
-    # scorpion:sys-scp-interesting_general-60s
-    (262, ['scorpion', '--search', 'astar(scp_online([projections(sys_scp(max_time=60, max_time_per_restart=6, max_pdb_size=2M, max_collection_size=20M, pattern_type=interesting_general), create_complete_transition_system=true)], saturator=perimstar, max_time=60, max_size=1M, interval=10K, orders=greedy_orders()))']),
-]
-
-CONFIGS_AXIOMS = [
-    # symk:sym-bd
-    (1513, ['symk', '--search', 'sym-bd(silent=true)']),
-    # scorpion:astar-blind
-    (1, ['scorpion', '--search', 'astar(blind())']),
+CONFIGS_ADL = [
+    # TODO GBFS-SCL (FF)
+    (720, ['fast-downward-conjunctions',  'TODO']),
+    # TODO YASP
+    (540, ['fast-downward-conjunctions', "TODO"]),
+    # TODO LAMA config
+    (540, ['fast-downward-conjunctions',  'TODO']),
 ]
 
 
-
-def get_pddl_features(task):
-    has_axioms = False
-    has_conditional_effects = False
-    with open(task) as f:
-        in_op = False
-        for line in f:
-            line = line.strip()
-            if line == "begin_rule":
-                has_axioms = True
-
-            if line == "begin_operator":
-                in_op = True
-            elif line == "end_operator":
-                in_op = False
-            elif in_op:
-                parts = line.split()
-                if len(parts) >= 6 and all(p.lstrip('-').isdigit() for p in parts):
-                    print(f"Task has at least one conditional effect: {line}")
-                    has_conditional_effects = True
-    return has_axioms, has_conditional_effects
-
-
-HAS_AXIOMS, HAS_CONDITIONAL_EFFECTS = get_pddl_features(SAS_FILE)
-
-print(f"Task has axioms: {HAS_AXIOMS}")
-print(f"Task has conditional effects: {HAS_CONDITIONAL_EFFECTS}")
-
-if HAS_AXIOMS:
-    CONFIGS = CONFIGS_AXIOMS
-elif HAS_CONDITIONAL_EFFECTS:
-    CONFIGS = CONFIGS_COND_EFFS
-else:
-    CONFIGS = CONFIGS_STRIPS
